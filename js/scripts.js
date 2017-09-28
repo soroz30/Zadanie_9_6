8f13ddf0ce1f79770c2fc65c3f5a1bf1da87b89e
soroz30 <soroz30@yahoo.pl>
2017-09-28 16:30:33 +0200

    Zadanie 9.6 correction

--- js/scripts.js
+++ js/scripts.js
@@ -7,5 +7,5 @@ window.onload = function() {
         var liNumber = document.getElementsByTagName('li').length;
         element.innerHTML = 'item ' + liNumber;
         list.appendChild(element);
-	});
+    });
 };
